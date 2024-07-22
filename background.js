caligraf/ChangeQuote

commit b6bb418b376c7ca6e2f67fa792e11067d9cf0230

Migration to TB 115 (#40)
remove windows listener API
use json instead of dtd for localization
use webExtension API
use browser storage instead of TB preferences

compose.js
@@ -1,0 +1,190 @@
+async function getPrefInStorage(prefName, defaultValue) {
+    let prefObj = await browser.storage.local.get(prefName);
+    if (prefObj && prefObj[prefName] != null )
+        return prefObj[prefName];
+    return defaultValue;
+}
+
+/**
+ * Update the message reply
+ */
+async function updateMessage(composeDetails, messageHeader, messagePart) {
+    try {
+        if (composeDetails.type == "reply") {
+            let identityId = await browser.runtime.sendMessage({
+                    command: "getIdentityId",
+                    options: {
+                        messageId: composeDetails.relatedMessageId
+                    }
+                });
+            let prefAutoQuote = "changequote." + identityId + ".auto_quote";
+            let quoted = await getPrefInStorage(prefAutoQuote, true);
+            if (!quoted) { // no quote
+                document.body.innerHTML = "<p></br><p>";
+            } else {
+                let inner = document.body.innerHTML;
+
+                if (inner.length < 20) // || inner.indexOf("([[)") < 0)
+                    return;
+
+                let sameFormatAsReceived = await getPrefInStorage("changequote.replyformat.enable");
+                if (sameFormatAsReceived) {
+                    let details = {};
+                    let msgFormat = messagePart.headers["content-type"];
+                    for (let i = 0; i < msgFormat.length; i++) {
+                        if (msgFormat[i].indexOf("plain") > -1) {
+                            details.deliveryFormat = 'plaintext';
+                            details.isPlainText = true;
+                            break;
+                        } else if (msgFormat[i].indexOf("alternative") > -1) {
+                            let formatAlternative = await getPrefInStorage("changequote.replyformat.format");
+                            if (formatAlternative) {
+                                if (formatAlternative == 0) {
+                                    details.deliveryFormat = 'both';
+                                } else if (formatAlternative == 1) {
+                                    details.deliveryFormat = 'html';
+                                    details.isPlainText = false;
+                                } else if (formatAlternative == 2) {
+                                    details.deliveryFormat = 'plaintext';
+                                    details.isPlainText = true;
+                                }
+                            } else {
+                                details.deliveryFormat = 'auto';
+                            }
+                            break;
+                        } else if (msgFormat[i].indexOf("html") > -1) {
+                            details.deliveryFormat = 'plaintext';
+                            details.isPlainText = false;
+                            break;
+                        }
+                    }
+                    await browser.runtime.sendMessage({
+                                command: "updateComposeDetail",
+                                options: {
+                                    details
+                                }
+                            });
+                }
+                let receivedDate = "";
+                let received = messagePart.headers["received"];
+                if (received) {
+                    let size = received.length;
+                    if (size >= 1) {
+                        let receivedDateTab = received[size - 1].split(";");
+                        if (receivedDateTab.length > 1) {
+                            receivedDate = receivedDateTab[1];
+                        }
+                    }
+                }
+
+                let cqheaders_type = await getPrefInStorage("changequote.headers.type");
+                let cqheaders_news = await getPrefInStorage("changequote.set.headers.news");
+                let msgdate = messageHeader.date;
+
+                let folder = messageHeader.folder;
+                let isnntp = await browser.runtime.sendMessage({
+                        command: "isNntpAccount",
+                        options: {
+                            accountId: folder.accountId
+                        }
+                    });
+                let isStandardHeader = false;
+                let realnewhdr = '';
+                if (isnntp) {
+                    if (cqheaders_news)
+                        realnewhdr = await browser.runtime.sendMessage({
+                                command: "getHeader",
+                                options: {
+                                    custom: true,
+                                    isNNTP: isnntp,
+                                    cite: false,
+                                    msgDate: msgdate,
+                                    receivedDate: receivedDate,
+                                    messageHeader: messageHeader,
+                                    endline: "([[br) /(]])"
+                                }
+                            });
+                    else
+                        isStandardHeader = true;
+                } else {
+                    if (cqheaders_type == 0) {
+                        realnewhdr = await browser.runtime.sendMessage({
+                                command: "getHeader",
+                                options: {
+                                    custom: false,
+                                    isNNTP: isnntp,
+                                    cite: false,
+                                    msgDate: msgdate,
+                                    receivedDate: receivedDate,
+                                    messageHeader: messageHeader,
+                                    endline: "([[)br /(]])"
+                                }
+                            });
+                    } else if (cqheaders_type == 1)
+                        isStandardHeader = true;
+                    else
+                        realnewhdr = await browser.runtime.sendMessage({
+                                command: "getHeader",
+                                options: {
+                                    custom: true,
+                                    isNNTP: isnntp,
+                                    cite: false,
+                                    msgDate: msgdate,
+                                    receivedDate: receivedDate,
+                                    messageHeader: messageHeader,
+                                    endline: "([[)br /(]])"
+                                }
+                            });
+                }
+                if (!isStandardHeader) {
+                    inner = inner.replace(/\*\*\*\*\*\*\*\*\*\*\*\*HeaderChangeQuote\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/g, realnewhdr);
+                    inner = inner.replace(/\(\[\[\)/g, "<");
+                    inner = inner.replace(/\(\]\]\)/g, ">");
+                    document.body.innerHTML = inner;
+                }
+            }
+            let inlineImgRemove = await browser.runtime.sendMessage({
+                    command: "needToRemoveInlineImages"
+                });
+            if (inlineImgRemove) {
+                let elementImages = document.body.getElementsByTagName("img");
+                for (let i = 0; i < elementImages.length; i++) {
+                    let imgSrc = elementImages[i].getAttribute("src");
+                    if (imgSrc.indexOf("http://") == -1 && imgSrc.indexOf("https://") == -1)
+                        elementImages[i].remove();
+                }
+            }
+
+            let markread_after_reply = await getPrefInStorage("changequote.message.markread_after_reply");
+            if (markread_after_reply)
+                await browser.runtime.sendMessage({
+                    command: "markMsgRead"
+                });
+            let close_after_reply = await getPrefInStorage("changequote.window.close_after_reply");
+            if (close_after_reply)
+                await browser.runtime.sendMessage({
+                    command: "closeWindows"
+                });
+        } else if (composeDetails.type == "forward") {
+            let close_after_reply = await getPrefInStorage("changequote.window.close_after_reply");
+            if (close_after_reply)
+                await browser.runtime.sendMessage({
+                    command: "closeWindows"
+                });
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+async function main() {
+    let composeDetails = await browser.runtime.sendMessage({
+            command: "getComposeDetails"
+        });
+    let message = await browser.runtime.sendMessage({
+            command: "getMessage"
+        });
+    updateMessage(composeDetails, message.messageHeader, message.messagePart);
+}
+
+main();

api/LegacyPrefs/implementation.js
@@ -1,0 +1,213 @@
+/*
+ * This file is provided by the addon-developer-support repository at
+ * https://github.com/thundernest/addon-developer-support
+ *
+ * Version 1.10
+ * - adjusted to Thunderbird Supernova (Services is now in globalThis)
+ *
+ * Version 1.9
+ * - fixed fallback issue reported by Axel Grude
+ *
+ * Version 1.8
+ * - reworked onChanged event to allow registering multiple branches
+ *
+ * Version 1.7
+ * - add onChanged event
+ *
+ * Version 1.6
+ * - add setDefaultPref()
+ *
+ * Version 1.5
+ * - replace set/getCharPref by set/getStringPref to fix encoding issue
+ *
+ * Version 1.4
+ * - setPref() function returns true if the value could be set, otherwise false
+ *
+ * Version 1.3
+ * - add setPref() function
+ *
+ * Version 1.2
+ * - add getPref() function
+ *
+ * Author: John Bieling (john@thunderbird.net)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+var { ExtensionCommon } = ChromeUtils.import(
+  "resource://gre/modules/ExtensionCommon.jsm"
+);
+var { ExtensionUtils } = ChromeUtils.import(
+  "resource://gre/modules/ExtensionUtils.jsm"
+);
+var { ExtensionError } = ExtensionUtils;
+
+var Services = globalThis.Services || 
+  ChromeUtils.import("resource://gre/modules/Services.jsm").Services;
+
+
+var LegacyPrefs = class extends ExtensionCommon.ExtensionAPI {
+  getAPI(context) {
+
+    class LegacyPrefsManager {
+      constructor() {
+        this.observedBranches = new Map();
+        this.QueryInterface = ChromeUtils.generateQI([
+          "nsIObserver",
+          "nsISupportsWeakReference",
+        ])
+      }
+
+      addObservedBranch(branch, fire) {
+        return this.observedBranches.set(branch, fire);
+      }
+
+      hasObservedBranch(branch) {
+        return this.observedBranches.has(branch);
+      }
+
+      removeObservedBranch(branch) {
+        return this.observedBranches.delete(branch);
+      }
+
+      async observe(aSubject, aTopic, aData) {
+        if (aTopic == "nsPref:changed") {
+          let branch = [...this.observedBranches.keys()]
+            .reduce(
+              (p, c) => aData.startsWith(c) && (!p || c.length > p.length) ? c : p,
+              null
+            );
+          if (branch) {
+            let name = aData.substr(branch.length);
+            let value = await this.getLegacyPref(aData);
+            let fire = this.observedBranches.get(branch);
+            fire(name, value);
+          }
+        }
+      }
+
+      async getLegacyPref(
+        aName,
+        aFallback = null,
+        userPrefOnly = true
+      ) {
+        let prefType = Services.prefs.getPrefType(aName);
+        if (prefType == Services.prefs.PREF_INVALID) {
+          return aFallback;
+        }
+
+        let value = aFallback;
+        if (!userPrefOnly || Services.prefs.prefHasUserValue(aName)) {
+          switch (prefType) {
+            case Services.prefs.PREF_STRING:
+              value = Services.prefs.getStringPref(aName, aFallback);
+              break;
+
+            case Services.prefs.PREF_INT:
+              value = Services.prefs.getIntPref(aName, aFallback);
+              break;
+
+            case Services.prefs.PREF_BOOL:
+              value = Services.prefs.getBoolPref(aName, aFallback);
+              break;
+
+            default:
+              console.error(
+                `Legacy preference <${aName}> has an unknown type of <${prefType}>.`
+              );
+          }
+        }
+        return value;
+      }
+    }
+
+    let legacyPrefsManager = new LegacyPrefsManager();
+
+    return {
+      LegacyPrefs: {
+        onChanged: new ExtensionCommon.EventManager({
+          context,
+          name: "LegacyPrefs.onChanged",
+          register: (fire, branch) => {
+            if (legacyPrefsManager.hasObservedBranch(branch)) {
+              throw new ExtensionError(`Cannot add more than one listener for branch "${branch}".`)
+            }
+            legacyPrefsManager.addObservedBranch(branch, fire.sync);
+            Services.prefs
+              .getBranch(null)
+              .addObserver(branch, legacyPrefsManager);
+            return () => {
+              Services.prefs
+                .getBranch(null)
+                .removeObserver(branch, legacyPrefsManager);
+              legacyPrefsManager.removeObservedBranch(branch);
+            };
+          },
+        }).api(),
+
+        // only returns something, if a user pref value is set
+        getUserPref: async function (aName) {
+          return await legacyPrefsManager.getLegacyPref(aName);
+        },
+
+        // returns the default value, if no user defined value exists,
+        // and returns the fallback value, if the preference does not exist
+        getPref: async function (aName, aFallback = null) {
+          return await legacyPrefsManager.getLegacyPref(aName, aFallback, false);
+        },
+
+        clearUserPref: function (aName) {
+          Services.prefs.clearUserPref(aName);
+        },
+
+        // sets a pref
+        setPref: async function (aName, aValue) {
+          let prefType = Services.prefs.getPrefType(aName);
+          if (prefType == Services.prefs.PREF_INVALID) {
+            console.error(
+              `Unknown legacy preference <${aName}>, forgot to declare a default?.`
+            );
+            return false;
+          }
+
+          switch (prefType) {
+            case Services.prefs.PREF_STRING:
+              Services.prefs.setStringPref(aName, aValue);
+              return true;
+              break;
+
+            case Services.prefs.PREF_INT:
+              Services.prefs.setIntPref(aName, aValue);
+              return true;
+              break;
+
+            case Services.prefs.PREF_BOOL:
+              Services.prefs.setBoolPref(aName, aValue);
+              return true;
+              break;
+
+            default:
+              console.error(
+                `Legacy preference <${aName}> has an unknown type of <${prefType}>.`
+              );
+          }
+          return false;
+        },
+
+        setDefaultPref: async function (aName, aValue) {
+          let defaults = Services.prefs.getDefaultBranch("");
+          switch (typeof aValue) {
+            case "string":
+              return defaults.setStringPref(aName, aValue);
+            case "number":
+              return defaults.setIntPref(aName, aValue);
+            case "boolean":
+              return defaults.setBoolPref(aName, aValue);
+          }
+        },
+      },
+    };
+  }
+};

commit 9b40040b6d989f45351d29dea8387bf211f850ed

Fix custom sender's date formatting
Fix a regression from commit b6bb418b376c ("Migration to TB 115 (#40)").

Before that commit, we would retrieve the sender's date from the "Date:"
header [0]. Then we had a function, specific to sender's date, called
"decodeCustomizedDateSender()" and that would extract the right date,
time zone, and apply the custom formatting [1].

But after this commit, the add-on picks the sender's date from the
"Received:" header field instead [2]. This does not look correct: for
mailing lists for example this field seems to use the time zone from the
mailing list server rather than the one from the sender. So we lose the
information about the sender here already. And there seems to be a
second issue: when the add-on processes the date after that, it creates
a new "Date()" object from the collected string [3]. As far as I
understand, this "Date" object stores no information about the time zone
from the original string, it is just a time stamp that renders with
"toString()" into the user's time zone. So we don't have any info left
when trying to recover the time zone in "decodeCustomizedDate()" [4].

To fix this:

  - Revert getting sender's date from "Received:" header field to
    "Date:" field, to pick the right date.
  - Extract sender's time zone from receivedDateString and pass it to
    decodeCustomizedDate(), so we don't lose the time zone information
    by relying only on receiveDate.
  - Process all relevant date fields with DateTimeFormat() formatters,
    to take the time zone into account.

[0] https://github.com/caligraf/ChangeQuote/commit/b6bb418b376c7ca6e2f67fa792e11067d9cf0230#diff-1af7b99192b3daf6e19b1a644859de38c8c4cfa9e3f367addf0861821a99bc2fL385
[1] https://github.com/caligraf/ChangeQuote/commit/b6bb418b376c7ca6e2f67fa792e11067d9cf0230#diff-1af7b99192b3daf6e19b1a644859de38c8c4cfa9e3f367addf0861821a99bc2fL601

background.js
@@ -16,44 +16,77 @@ function CQcapitalize(val) {
     return newVal;
 }
 
-function decodeCustomizedDate(date, str, capitalizeDate) {
-    const optionsWeekday = { weekday: 'long' };
-    const weekdayformat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsWeekday);
-    let weekday = weekdayformat.format(date);
-    const optionsMonth = { month: 'long' };
-    const monthFormat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsMonth);
-    let month = monthFormat.format(date);
-    let d = date.getDate();
-    let e = d < 10 ? " " + d : d;
-    d = d < 10 ? "0" + d : d;
-    let m = date.getMonth() + 1;
-    m = m < 10 ? "0" + m : m;
-    let y = date.getYear() - 100;
-    y = y < 10 ? "0" + y : y;
-    let D = date.toString().split(" ")[0];
-    let M = date.toString().split(" ")[1];
-    let Y = date.getFullYear();
-    let i = date.getMinutes();
-    i = i < 10 ? "0" + i : i;
-    let s = date.getSeconds();
-    s = s < 10 ? "0" + s : s;
-    let H = date.getHours();
-    let h = H > 12 ? H - 12 : H == 0 ? 12 : H;
-    let a = H > 11 && H < 24 ? "pm" : "am";
-    let A = H > 11 && H < 24 ? "PM" : "AM";
-    let z = date.toString().split(" ")[5];
+function getTimeZone(str) {
+    let z = str.replace(/^ +/, "");
+    z = z.split(" ")[5];
     z = z.replace(/[a-zA-Z]+/, "");
     z = z.substring(0, 5);
-    H = H < 10 ? "0" + H : H;
-    h = h < 10 ? "0" + h : h;
-    if( capitalizeDate) {
-        weekday = CQcapitalize(weekday);
-        month = CQcapitalize(month);
+    return z;
+}
+
+function decodeCustomizedDate(date, str, timeZone, capitalizeDate) {
+    const optionsWeekday = { weekday: 'long', timeZone: timeZone };
+    const weekdayformat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsWeekday);
+    let LD = weekdayformat.format(date);
+
+    const optionsMonthName = { month: 'long', timeZone: timeZone };
+    const monthNameFormat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsMonthName);
+    let LM = monthNameFormat.format(date);
+
+    const optionsDayEnShort = { weekday: 'short', timeZone: timeZone };
+    const dayEnShortFormat = new Intl.DateTimeFormat('en-US', optionsDayEnShort);
+    let D = dayEnShortFormat.format(date);
+
+    const optionsMonthEnShort = { month: 'short', timeZone: timeZone };
+    const monthEnShortFormat = new Intl.DateTimeFormat('en-US', optionsMonthEnShort);
+    let M = monthEnShortFormat.format(date);
+
+    const optionsDay = { day: 'numeric', timeZone: timeZone };
+    const dayFormat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsDay);
+    let d = dayFormat.format(date);
+    let e = d < 10 ? " " + d : d;
+    d = d < 10 ? "0" + d : d;
+
+    const optionsMonth = { month: '2-digit', timeZone: timeZone };
+    const monthFormat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsMonth);
+    let m = monthFormat.format(date);
+
+    const optionsYear = { year: '2-digit', timeZone: timeZone };
+    const yearFormat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsYear);
+    let y = yearFormat.format(date);
+
+    const optionsYearFull = { year: 'numeric', timeZone: timeZone };
+    const yearFullFormat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsYearFull);
+    let Y = yearFullFormat.format(date);
+
+    const optionsMinutes = { minute: '2-digit', timeZone: timeZone };
+    const minutesFormat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsMinutes);
+    let i = minutesFormat.format(date);
+
+    const optionsSeconds = { second: '2-digit', timeZone: timeZone };
+    const secondsFormat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsSeconds);
+    let s = secondsFormat.format(date);
+
+    const optionsHours12 = { hour: '2-digit', hour12: true, timeZone: timeZone };
+    const hours12Format = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsHours12);
+    let h = hours12Format.format(date).split(" ")[0];
+
+    const optionsHours = { hour: '2-digit', hour12: false, timeZone: timeZone };
+    const hoursFormat = new Intl.DateTimeFormat(messenger.i18n.getUILanguage(), optionsHours);
+    let H = hoursFormat.format(date);
+
+    let a = Number(H) > 11 && Number(H) < 24 ? "pm" : "am";
+    let A = Number(H) > 11 && Number(H) < 24 ? "PM" : "AM";
+    let z = timeZone;
+    if (capitalizeDate) {
+        LD = CQcapitalize(LD);
+        LM = CQcapitalize(LM);
         D = CQcapitalize(D);
         M = CQcapitalize(M);
     }
-    str = str.replace("%LM", month);
-    str = str.replace("%LD", weekday);
+
+    str = str.replace("%LM", LM);
+    str = str.replace("%LD", LD);
     str = str.replace("%d", d);
     str = str.replace("%D", D);
     str = str.replace("%m", m);
@@ -97,13 +130,17 @@ async function CQgetDate(headerDate, receivedDateString) {
             datestring = headerDate.toString();
         else if (dateLongFormat == 3) { //local : receiver date
             let str = await getPrefInStorage("changequote.headers.date_custom_format");
+            let timeZone = getTimeZone(headerDate.toString());
             postCapitalization = false;
-            datestring = decodeCustomizedDate(headerDate, str, changequote_headers_capitalize_date);
+            datestring = decodeCustomizedDate(headerDate, str, timeZone,
+                                              changequote_headers_capitalize_date);
         } else if (dateLongFormat == 4) { //sender date
             postCapitalization = false;
             let str = await getPrefInStorage("changequote.headers.dateSender_custom_format");
             let receivedDate = new Date(receivedDateString)
-                datestring = decodeCustomizedDate(receivedDate, str, changequote_headers_capitalize_date);
+            let timeZone = getTimeZone(receivedDateString);
+            datestring = decodeCustomizedDate(receivedDate, str, timeZone,
+                                              changequote_headers_capitalize_date);
         } else
             datestring = headerDate.toLocaleString();
         if (changequote_headers_capitalize_date && postCapitalization)

compose.js
@@ -84,14 +84,11 @@ async function updateMessage(composeDetails, messageHeader, messagePart) {
                     });
                 }
                 let receivedDate = "";
-                let received = messagePart.headers["received"];
+                let received = messagePart.headers["date"];
                 if (received) {
                     let size = received.length;
                     if (size >= 1) {
-                        let receivedDateTab = received[size - 1].split(";");
-                        if (receivedDateTab.length > 1) {
-                            receivedDate = receivedDateTab[1];
-                        }
+                        receivedDate = received[0];
                     }
                 }
 